62efec35af72387ad050470942acd0017783c5ba
anaesparapani <anaclaraesparapani@gmail.com>
2025-02-12 13:36:24 -0300

    12-02

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -7,6 +7,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
+import api from "../axios/axios";
 
 function Login() {
   const [user, setUser] = useState({
@@ -15,13 +16,25 @@ function Login() {
   });
 
   const onChange = (event) => {
-    const{name, value} = event.target;
-    setUser({...user,[name]:value})
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
   };
 
-  const handleSubmit = (event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    alert("Email:"+user.email+" "+"Senha:"+user.password)
+    login();
+  };
+
+  async function login() {
+    await api.postLogin(user).then(
+      (response) =>{
+        alert(response.data.message)
+      },
+      (error) =>{
+        console.log(error)
+        alert(error.response.data.error)
+      }
+    )
   }
 
   return (
@@ -40,7 +53,12 @@ function Login() {
         <Typography component="h1" variant="h5">
           Vio
         </Typography>
-        <Box component="form" sx={{ marginTop: 1 }} onSubmit={handleSubmit} noValidate>
+        <Box
+          component="form"
+          sx={{ marginTop: 1 }}
+          onSubmit={handleSubmit}
+          noValidate
+        >
           <TextField
             required
             fullWidth
